18c4db570a91e302514b2edef57b30ae770d92be
Haley Sanders <haleyfsanders@gmail.com>
2017-03-19 16:15:31 -0700

    updates to correlation_parser and renamed StatePop

--- js/chord.js
+++ js/chord.js
@@ -1,29 +1,129 @@
 
-var chord_svg = d3.select("#chord-chart").append("svg")
-				.attr("width",width + 700)
-				.attr("height",height + 700),
-			    outerRadius = Math.min(width + 300, height + 300) * 0.5 - 40,
-			    innerRadius = outerRadius - 30;
+var loadChord = function(){
+	var chord_width = 500,
+	chord_height = 500,
+	chord_matrix_dim = CategoryNames.length;
 
-//chord_matrix = [Access, Education, Health, Poverty]
+	var chord_svg = d3.select("#chord-chart").append("svg")
+		.attr("width",chord_width + 700)
+		.attr("height",chord_height + 700),
+	    outerRadius = Math.min(chord_width + 300, chord_height + 300) * 0.5 - 40,
+	    innerRadius = outerRadius - 30;
 
-// Access:
-	//Access
-	//Stores
-	//Restaurants
-	//LocalFoodAccess
-	//LocalFoodProduction
 
-// Education:
-	//Education
-	//StudentAssistance
+	//Setting up the matrix	
+	var chord_matrix = []
 
-//Health:
-	//Health
+	for(i in d3.range(chord_matrix_dim)){
+		chord_matrix[i] = []
 
-//Poverty
-	//FoodCosts
-	//Insecurity
-	//Assistance
-	
-var chord_matrix = []
+		for(k in d3.range(chord_matrix_dim)){
+			chord_matrix[i][k] = 0
+		}
+	}
+
+	var cat_id = 0 //id of the category, so Access has cat_id == 0,Assistance has cat_id == 1
+	var var_id = 0
+
+	for(cat in categoryObject){
+		var set_array = Array.from(categoryObject[cat])
+		for(collection in set_array){
+			var cur_var = set_array[collection]
+			
+			// if(cur_var)
+				// 	chord_matrix[cat_id][var_id] = 
+			// 	var_id += 1
+		}
+		// for(variable in category){
+
+	}
+
+		// // cat_id += 1
+		// // var_id = 0
+	// }
+
+	var chord_svg = d3.select("#chord-chart").append("svg")
+		.attr("width",chord_width + 700)
+		.attr("height",chord_height + 700),
+	    outerRadius = Math.min(chord_width + 300, chord_height + 300) * 0.5 - 40,
+	    innerRadius = outerRadius - 30;
+
+	chord_svg.selectAll("*").remove()
+	var formatValue = d3.formatPrefix(",.0", 1e3);
+
+	var chord = d3.chord()
+	    .padAngle(0.05)
+	    .sortSubgroups(d3.descending);
+
+	// var arc = d3.arc()
+	//     .innerRadius(innerRadius)
+	//     .outerRadius(outerRadius);
+
+	// var ribbon = d3.ribbon()
+	//     .radius(innerRadius);
+	    
+
+	// var color = d3.scaleOrdinal()
+	//     .domain(d3.range(91))
+	//     .range(["#000000", "#FFDD89", "#957244", "#F26223"]);
+
+	// var g = chord_svg.append("g")
+	//     .attr("transform", "translate(" + ((width / 2)  + 200) + "," + ((height / 2)  + 145) + ")")
+	//     .datum(chord(matrix));
+
+	// var group = g.append("g")
+	//     .attr("class", "groups")
+	//   .selectAll("g")
+	//   .data(function(chords) { return chords.groups; })
+	//   .enter().append("g");
+
+	// group.append("path")
+	//     .style("fill", function(d) { return color(d.index); })
+	//     .style("stroke", function(d) { return d3.rgb(color(d.index)).darker(); })
+	//     .attr("d", arc);
+
+	// var groupTick = group.selectAll(".group-tick")
+	//   .data(function(d) { return groupTicks(d, 1e3); })
+	//   .enter().append("g")
+	//     .attr("class", "group-tick")
+	//     .attr("transform", function(d) { return "rotate(" + (d.angle * 180 / Math.PI - 90) + ") translate(" + outerRadius + ",0)"; });
+
+	// groupTick.append("line")
+	//     .attr("x2", 6);
+
+	// var tick_label = 1;
+	// groupTick
+	//   .filter(function(d) { return d.value % 5e3 === 0; })
+	//   .append("text")
+	//     .attr("x", 8)
+	//     .attr("dy", ".35em")
+	//     .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180) translate(-16)" : null; })
+	//     .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+	//     .text(function(k) {
+	//     	tick_label += 1
+	//     	return tick_label % 91
+
+	    	
+	//     });
+
+	// g.append("g")
+	//     .attr("class", "ribbons")
+	//   .selectAll("path")
+	//   .data(function(chords) { return chords; })
+	//   .enter().append("path")
+	//     .attr("d", ribbon)
+	//     .style("fill", function(d) { return color(d.target.index); })
+	//     .style("stroke", function(d) { return d3.rgb(color(d.target.index)).darker(); });
+
+	// // Returns an array of tick angles and values for a given group and step.
+	// function groupTicks(d, step) {
+	//   var k = (d.endAngle - d.startAngle) / d.value;
+	//   return d3.range(0, d.value, step).map(function(value) {
+	//     return {value: value, angle: value * k + d.startAngle};
+	//   });
+	// }
+	// });
+
+	// }
+
+}
--- js/correlation_parser.js
+++ js/correlation_parser.js
@@ -1,31 +1,53 @@
-correlation_csv = "./data/corr.csv"
+loadCorrParser = function() {
+	correlation_csv = "../data/corr.csv"
 
-var correlation_dict = {}
+	var correlation_dict = {}
 
-d3.csv(correlation_csv,function(corr_data){
+	d3.csv(correlation_csv,function(corr_data){
 
-	corr_data.forEach(function(cd){
-		cur_row = null
-		cur_key = null
-		for(key in cd){
-			if(key == ""){ 
-				cur_row = cd[key] // cur_row is the left most cell on the row
-				if(!(cur_row in correlation_dict) ){
-					correlation_dict[cur_row] = {}
+		corr_data.forEach(function(cd){
+			cur_row = null
+			cur_key = null
+			for(key in cd){
+
+				if(key == ""){ 
+					cur_row = cd[key] // cur_row is the left most cell on the row
+					if(!(cur_row in correlation_dict) ){
+						correlation_dict[cur_row] = {}
+					}
 				}
+				else{
+
+					cur_key = key
+					var same_cat = false //tracks whether or not curkey and cur_row are in the same category
+					for(cat in categoryObject){
+						var set_array = Array.from(categoryObject[cat])
+
+						if(set_array.indexOf(cur_key) > -1){
+							if(set_array.indexOf(cur_row) > - 1){
+								same_cat = true
+								break
+							}
+						}
+					}
+					
+					if(!same_cat){
+						if(!(cur_key in correlation_dict)){
+							//cur_key is the column being compared to the current row
+							correlation_dict[cur_row][cur_key] = cd[cur_key]
+						}
+						
+						if(cur_key == cur_row){//correlation to itself i.e. Unemployment to Unemployment
+							correlation_dict[cur_row]["SELF"] = cd[cur_key]
+						}	
+					}
+					
+				}
+
 			}
-			else{
-				cur_key = key
-				if(!(cur_key in correlation_dict)){
-					//cur_key is the column being compared to the current row
-					correlation_dict[cur_row][cur_key] = cd[cur_key]
-				}
-				
-				if(cur_key == cur_row){//correlation to itself i.e. Unemployment to Unemployment
-					correlation_dict[cur_row]["SELF"] = cd[cur_key]
-				}
-			}
+		});
 
-		}
+		console.log(correlation_dict)
 	});
-});
+
+}
