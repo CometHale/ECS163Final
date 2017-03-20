6221704cbf56015fa3ea56a0c65dd1ca76b4393d
Sally Ly <sadly@ucdavis.edu>
2017-03-19 21:11:20 -0700

    moved files

---
+++ js/menu.js
@@ -0,0 +1,157 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction1() {
+    document.getElementById("myDropdown1").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown1 = document.getElementById("myDropdown1");
+      if (myDropdown1.classList.contains('show')) {
+        myDropdown1.classList.remove('show');
+      }
+  }
+}
+}
+
+function myFunction2() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown2 = document.getElementById("myDropdown2");
+      if (myDropdown2.classList.contains('show')) {
+        myDropdown2.classList.remove('show');
+      }
+  }
+}
+}
+
+function myFunction3() {
+    document.getElementById("myDropdown3").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown3 = document.getElementById("myDropdown3");
+      if (myDropdown3.classList.contains('show')) {
+        myDropdown3.classList.remove('show');
+      }
+  }
+}
+}
+
+function myFunction4() {
+    document.getElementById("myDropdown4").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown4 = document.getElementById("myDropdown4");
+      if (myDropdown4.classList.contains('show')) {
+        myDropdown4.classList.remove('show');
+      }
+  }
+}
+}
+
+function myFunction5() {
+    document.getElementById("myDropdown5").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown5 = document.getElementById("myDropdown5");
+      if (myDropdown5.classList.contains('show')) {
+        myDropdown5.classList.remove('show');
+      }
+  }
+}
+}
+
+function myFunction6() {
+    document.getElementById("myDropdown6").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown6 = document.getElementById("myDropdown6");
+      if (myDropdown6.classList.contains('show')) {
+        myDropdown6.classList.remove('show');
+      }
+  }
+}
+}
+
+function myFunction7() {
+    document.getElementById("myDropdown7").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown7 = document.getElementById("myDropdown7");
+      if (myDropdown7.classList.contains('show')) {
+        myDropdown7.classList.remove('show');
+      }
+  }
+}
+}
+
+function myFunction8() {
+    document.getElementById("myDropdown8").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown8 = document.getElementById("myDropdown8");
+      if (myDropdown8.classList.contains('show')) {
+        myDropdown8.classList.remove('show');
+      }
+  }
+}
+}
+
+function myFunction9() {
+    document.getElementById("myDropdown9").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown9 = document.getElementById("myDropdown9");
+      if (myDropdown9.classList.contains('show')) {
+        myDropdown9.classList.remove('show');
+      }
+  }
+}
+}
+
+function myFunction10() {
+    document.getElementById("myDropdown10").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown10 = document.getElementById("myDropdown10");
+      if (myDropdown10.classList.contains('show')) {
+        myDropdown10.classList.remove('show');
+      }
+  }
+}
+}
+
+function myFunction11() {
+    document.getElementById("myDropdown11").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown11 = document.getElementById("myDropdown11");
+      if (myDropdown11.classList.contains('show')) {
+        myDropdown11.classList.remove('show');
+      }
+  }
+}
+}
+
+function myFunction12() {
+    document.getElementById("myDropdown12").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown12 = document.getElementById("myDropdown12");
+      if (myDropdown12.classList.contains('show')) {
+        myDropdown12.classList.remove('show');
+      }
+  }
+}
+}
+
+function myFunction13() {
+    document.getElementById("myDropdown13").classList.toggle("show");
+    window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown13 = document.getElementById("myDropdown13");
+      if (myDropdown13.classList.contains('show')) {
+        myDropdown13.classList.remove('show');
+      }
+  }
+}
+}
