15400243226a876748b06cb46271a2a4f824d48b
Haley Sanders <haleyfsanders@gmail.com>
2017-03-19 23:29:23 -0700

    updates to chord and correlation parser

--- js/chord.js
+++ js/chord.js
@@ -1,129 +1,149 @@
 
 var loadChord = function(){
-	var chord_width = 500,
-	chord_height = 500,
-	chord_matrix_dim = CategoryNames.length;
 
-	var chord_svg = d3.select("#chord-chart").append("svg")
-		.attr("width",chord_width + 700)
-		.attr("height",chord_height + 700),
-	    outerRadius = Math.min(chord_width + 300, chord_height + 300) * 0.5 - 40,
-	    innerRadius = outerRadius - 30;
 
 
 	//Setting up the matrix	
 	var chord_matrix = []
+	var variable_array = []
+	var category_var_array = {}
+	var category_color = ["#5B2163","#1F255C","#BF7140","#3F9D34","#674822","#A13636","#349D99","#C95EAD","#8C81D5","#87D47D","#C0CC66","#2E6D84"]
 
-	for(i in d3.range(chord_matrix_dim)){
-		chord_matrix[i] = []
-
-		for(k in d3.range(chord_matrix_dim)){
-			chord_matrix[i][k] = 0
-		}
-	}
-
-	var cat_id = 0 //id of the category, so Access has cat_id == 0,Assistance has cat_id == 1
-	var var_id = 0
+// Access: #5B2163
+// Assistance: #1F255C
+// Demographics: #BF7140
+// Economic: #3F9D34
+// Food Costs: #674822
+// Health: #A13636
+// Insecurity: #349D99
+// Local Food Access: #C95EAD
+// Local Food Production: #8C81D5
+// Restaurants: #87D47D
+// Stores: #C0CC66
+// Student Assistance: #2E6D84
 
 	for(cat in categoryObject){
 		var set_array = Array.from(categoryObject[cat])
+
+		if(!(cat in category_var_array)){
+			category_var_array[cat] = []
+		}
+
 		for(collection in set_array){
 			var cur_var = set_array[collection]
-			
-			// if(cur_var)
-				// 	chord_matrix[cat_id][var_id] = 
-			// 	var_id += 1
+
+			category_var_array[cat].push(cur_var)
+			if(variable_array.indexOf(cur_var) == -1){
+				variable_array.push(cur_var)
+			}
+
+			var cur_var_id = variable_array.indexOf(cur_var)
+			chord_matrix[cur_var_id] = []
 		}
-		// for(variable in category){
 
 	}
 
-		// // cat_id += 1
-		// // var_id = 0
-	// }
+	for(cur_id in variable_array){
+		var cur_var = variable_array[cur_id]
+		for(correl_var in correlation_dict[cur_var]){
+			var correl_var_id = variable_array.indexOf(correl_var)
+			var cor_val = correlation_dict[cur_var][correl_var]
+
+			if(cor_val < 0){
+				cor_val = cor_val * -1 
+			}
+
+			cor_val =  100 % cor_val
+			chord_matrix[cur_id][correl_var_id] = cor_val
+		}
+	}
+
+	var chord_width = 1200,
+		chord_height = 1200,
+		chord_matrix_dim = CategoryNames.length,
+		outerRadius = Math.min(chord_width, chord_height) * 0.5 - 40,
+		innerRadius = outerRadius - 30;
 
 	var chord_svg = d3.select("#chord-chart").append("svg")
-		.attr("width",chord_width + 700)
-		.attr("height",chord_height + 700),
-	    outerRadius = Math.min(chord_width + 300, chord_height + 300) * 0.5 - 40,
-	    innerRadius = outerRadius - 30;
+						.attr("width",chord_width)
+						.attr("height",chord_height);
 
-	chord_svg.selectAll("*").remove()
 	var formatValue = d3.formatPrefix(",.0", 1e3);
 
 	var chord = d3.chord()
 	    .padAngle(0.05)
 	    .sortSubgroups(d3.descending);
 
-	// var arc = d3.arc()
-	//     .innerRadius(innerRadius)
-	//     .outerRadius(outerRadius);
+	var arc = d3.arc()
+	    .innerRadius(innerRadius)
+	    .outerRadius(outerRadius);
 
-	// var ribbon = d3.ribbon()
-	//     .radius(innerRadius);
-	    
+	var ribbon = d3.ribbon()
+	    .radius(innerRadius);
 
-	// var color = d3.scaleOrdinal()
-	//     .domain(d3.range(91))
-	//     .range(["#000000", "#FFDD89", "#957244", "#F26223"]);
+	var color = d3.scaleOrdinal()
+	    .domain(d3.range(13))
+	    .range(category_color);
 
-	// var g = chord_svg.append("g")
-	//     .attr("transform", "translate(" + ((width / 2)  + 200) + "," + ((height / 2)  + 145) + ")")
-	//     .datum(chord(matrix));
+	var g = chord_svg.append("g")
+	    .attr("transform", "translate(" + chord_width / 2 + "," + chord_height / 2 + ")")
+	    .datum(chord(chord_matrix));
 
-	// var group = g.append("g")
-	//     .attr("class", "groups")
-	//   .selectAll("g")
-	//   .data(function(chords) { return chords.groups; })
-	//   .enter().append("g");
+	var group = g.append("g")
+	    .attr("class", "groups")
+	  .selectAll("g")
+	  .data(function(chords) { return chords.groups; })
+	  .enter().append("g");
 
-	// group.append("path")
-	//     .style("fill", function(d) { return color(d.index); })
-	//     .style("stroke", function(d) { return d3.rgb(color(d.index)).darker(); })
-	//     .attr("d", arc);
+	group.append("path")
+	    .style("fill", function(d) { 
+	    	var index = 0
+	    	for(categ in category_var_array){
+	    		var variable = variable_array[d.index]
 
-	// var groupTick = group.selectAll(".group-tick")
-	//   .data(function(d) { return groupTicks(d, 1e3); })
-	//   .enter().append("g")
-	//     .attr("class", "group-tick")
-	//     .attr("transform", function(d) { return "rotate(" + (d.angle * 180 / Math.PI - 90) + ") translate(" + outerRadius + ",0)"; });
+	    		if(category_var_array[categ].indexOf(variable) > - 1){
+	    			console.log("")
+	    			break
+	    		}
+	    		index += 1;	
+	    	}
+	    	return color(index); 
+	    })
+	    .style("stroke", function(d) { return d3.rgb(color(d.index)).darker(); })
+	    .attr("d", arc);
 
-	// groupTick.append("line")
-	//     .attr("x2", 6);
+	var groupTick = group.selectAll(".group-tick")
+	  .data(function(d) { return groupTicks(d, 1e3); })
+	  .enter().append("g")
+	    .attr("class", "group-tick");
+	    // .attr("transform", function(d) { return "rotate(" + (d.angle * 180 / Math.PI - 90) + ") translate(" + outerRadius + ",0)"; });
 
-	// var tick_label = 1;
-	// groupTick
-	//   .filter(function(d) { return d.value % 5e3 === 0; })
-	//   .append("text")
-	//     .attr("x", 8)
-	//     .attr("dy", ".35em")
-	//     .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180) translate(-16)" : null; })
-	//     .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
-	//     .text(function(k) {
-	//     	tick_label += 1
-	//     	return tick_label % 91
+	groupTick.append("line")
+	    .attr("x2", 6);
 
-	    	
-	//     });
+	groupTick
+	  .filter(function(d) { return d.value % 5e3 === 0; })
+	  .append("text")
+	    .attr("x", 8)
+	    .attr("dy", ".35em")
+	    .attr("transform", function(d) { return d.angle > Math.PI ? "rotate(180) translate(-16)" : null; })
+	    .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+	    .text(function(d) { return formatValue(d.value); });
 
-	// g.append("g")
-	//     .attr("class", "ribbons")
-	//   .selectAll("path")
-	//   .data(function(chords) { return chords; })
-	//   .enter().append("path")
-	//     .attr("d", ribbon)
-	//     .style("fill", function(d) { return color(d.target.index); })
-	//     .style("stroke", function(d) { return d3.rgb(color(d.target.index)).darker(); });
+	g.append("g")
+	    .attr("class", "ribbons")
+	  .selectAll("path")
+	  .data(function(chords) { return chords; })
+	  .enter().append("path")
+	    .attr("d", ribbon)
+	    .style("fill", function(d) { return color(d.target.index); })
+	    .style("stroke", function(d) { return d3.rgb(color(d.target.index)).darker(); });
 
-	// // Returns an array of tick angles and values for a given group and step.
-	// function groupTicks(d, step) {
-	//   var k = (d.endAngle - d.startAngle) / d.value;
-	//   return d3.range(0, d.value, step).map(function(value) {
-	//     return {value: value, angle: value * k + d.startAngle};
-	//   });
-	// }
-	// });
-
-	// }
-
+	// Returns an array of tick angles and values for a given group and step.
+	function groupTicks(d, step) {
+	  var k = (d.endAngle - d.startAngle);
+	  return d3.range(0, d.value, step).map(function(value) {
+	    return {value: value, angle: value * k + d.startAngle};
+	  });
+	}
 }
--- js/correlation_parser.js
+++ js/correlation_parser.js
@@ -1,7 +1,10 @@
-var correlation_dict = {}
+
+var correlation_dict = {} // correlations for the variables
+var category_correlations = {} // correlations calculated based on the averaged absolute values of the variable correlations
+var cat_var_dict = {}
 
 loadCorrParser = function() {
-	correlation_csv = "data/corr.csv"
+	correlation_csv = "../data/corr.csv"
 
 	d3.csv(correlation_csv,function(corr_data){
 
@@ -25,6 +28,8 @@ loadCorrParser = function() {
 
 						if(set_array.indexOf(cur_key) > -1){
 							if(set_array.indexOf(cur_row) > - 1){
+
+								correlation_dict[cur_row][cur_key] = 0 // no correlation between variables under the same category
 								same_cat = true
 								break
 							}
@@ -32,22 +37,65 @@ loadCorrParser = function() {
 					}
 					
 					if(!same_cat){
-						if(!(cur_key in correlation_dict)){
+						// if(!(cur_key in correlation_dict[cur_row])){
 							//cur_key is the column being compared to the current row
-							correlation_dict[cur_row][cur_key] = cd[cur_key]
-						}
+						correlation_dict[cur_row][cur_key] = cd[cur_key]
+						// }
 						
-						if(cur_key == cur_row){//correlation to itself i.e. Unemployment to Unemployment
-							correlation_dict[cur_row]["SELF"] = cd[cur_key]
-						}	
+						// if(cur_key == cur_row){//correlation to itself i.e. Unemployment to Unemployment
+						// 	correlation_dict[cur_row]["SELF"] = cd[cur_key]
+						// }	
 					}
 					
 				}
 
 			}
 		});
+		
+		for(catego in categoryObject){
+			var array = Array.from(categoryObject[catego])
+			cat_var_dict[catego] = []
+			category_correlations[catego] = {}
+			for(collection in array){
+				var cur_var = array[collection]
+				cat_var_dict[catego].push(cur_var)
+			}
 
-		// console.log(correlation_dict)
+		}
+
+		
+	});
+	
+	calcCategoryCorrelations();
+	
+}
+
+calcCategoryCorrelations = function(){
+
+	for(correla in correlation_dict){
+		console.log(correla)
+		for(cate in cat_var_dict){
+			
+			if(cat_var_dict[cate].indexOf(correla) > -1){
+
+				for(sub_key in correlation_dict[correla]){
+
+					if(!(sub_key in category_correlations[cate])){
+						category_correlations[cate][sub_key] = 0
+					}
+					category_correlations[cate][sub_key] += category_correlations[cate][sub_key]
+					console.log("here")
+				}
+			}
+
+		}
+	}
+
+	d3.json("../data/us-10m.v1.json", function(usGeo) {
+	  loadChord();
 	});
 
+	
 }
+
+loadCorrParser();
