217b328398294be42e9a1417c5660dbb15494be6
kctanaka <kctanaka@ucdavis.edu>
2017-03-20 18:49:01 -0700

    Update population_parser.js

--- js/population_parser.js
+++ js/population_parser.js
@@ -7,14 +7,12 @@ function LoadPops()
   const county_csv = "data/CountyPop.csv"
 
   d3.csv(state_csv, function(state_data){
-    console.log(state_data);
 	  state_data.forEach(function(s){
 		  populationByState[s.State] = s.StatePopulation;
 	  });
   });
 
   d3.csv(county_csv, function(county_data){
-    console.log(county_data);
 		county_data.forEach(function(c){
 		  populationByCounty[+c.FIPSCode] = [c.State, c.CountyName,+c.PopulationEstimate_2012];
 		});
